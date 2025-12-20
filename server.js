2ac332ebfc25895e3b36effa4a088b71f00a3ea7
NakshatraNamahaCreations <ops.nnc@gmail.com>
2025-12-20 10:37:36 +0530

    updated code

--- server.js
+++ server.js
@@ -46,6 +46,8 @@ app.use("/api/admin/blogs", adminBlogRoutes);
 app.use("/api/blogs", blogRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
