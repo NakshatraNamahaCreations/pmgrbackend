eea61c90b90e1b0cedb1a84b3eda2e4de5720ac4
NakshatraNamahaCreations <ops.nnc@gmail.com>
2025-12-20 13:11:04 +0530

    latest code

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -14,7 +14,7 @@ export const getAllBlogs = async (req, res) => {
     const blogs = await Blog.find(filter)
       .sort({ createdAt: -1 })
       .select(
-        "city title metaTitle metaDescription bannerImage extraImage services redirectLink createdAt"
+        "city title description metaTitle metaDescription bannerImage extraImage faqs redirectLink createdAt"
       );
 
     res.json({ success: true, blogs });
